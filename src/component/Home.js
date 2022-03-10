4e0a7889ba6732d590a57430a14982e9cda2f09d
NicoSurbayrole <nico.surba@hotmail.com>
2022-03-10 18:50:31 -0300

    nav+log+reg v2

---
+++ src/component/Home.js
@@ -0,0 +1,15 @@
+import Carousel from "./Carousel";
+import Grid from "./Grid";
+
+const Home = () => {
+  return (
+    <div>
+      <Carousel />
+      <div>
+        <Grid />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
--- src/component/Login.js
+++ src/component/Login.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-import useInput from "../hooks/hook";
+import { setUser } from "../state/user";
+import { Link as Linked } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import {
@@ -19,23 +21,30 @@ import {
   InputRightElement,
 } from "@chakra-ui/react";
 import { FaUserAlt, FaLock } from "react-icons/fa";
+import { useDispatch } from "react-redux";
 
 const CFaUserAlt = chakra(FaUserAlt);
 const CFaLock = chakra(FaLock);
 
-
 const Login = () => {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
   const [showPassword, setShowPassword] = useState(false);
-  const hookEmail = useInput();
-  const hookPassword = useInput();
-  const handleShowClick = () => setShowPassword(!showPassword);
-  
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
 
-  const onSubmit = (value) =>{
-  axios.post("api/login",value)
-  console.log(value);
-}
+  const handleShowClick = () => setShowPassword(!showPassword);
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+      axios.post('/login',data)
+      .then(user => dispatch(setUser(user.data)))
+      .then(navigate("/"))  
+  };
 
   return (
     <Flex
@@ -69,11 +78,10 @@ const Login = () => {
                     children={<CFaUserAlt color="gray.300" />}
                   />
                   <Input
-                    {...register("email",{required:true})}
+                    {...register("email", { required: true })}
                     type="email"
                     placeholder="email address"
-                    />
-                    
+                  />
                 </InputGroup>
               </FormControl>
               <FormControl>
@@ -84,11 +92,11 @@ const Login = () => {
                     children={<CFaLock color="gray.300" />}
                   />
                   <Input
-                    {...register("password",{required:true})}
+                    {...register("password", { required: true })}
                     type={showPassword ? "text" : "password"}
                     placeholder="Password"
                   />
-                  
+
                   <InputRightElement width="4.5rem">
                     <Button h="1.75rem" size="sm" onClick={handleShowClick}>
                       {showPassword ? "Esconder" : "Mostrar"}
@@ -117,9 +125,9 @@ const Login = () => {
       </Stack>
       <Box>
         Eres nuevo?{" "}
-        <Link color="blue.500" href="/Registro">
-          Registrarse
-        </Link>
+        <Linked to="/register">
+          <Link color="blue.500">Registrarse</Link>
+        </Linked>
       </Box>
     </Flex>
   );

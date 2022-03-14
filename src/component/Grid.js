7a8a7bcd66da4df3e8340de145b61a0545d69793
santiagocata <santiagoezequielcata@gmail.com>
2022-03-14 18:28:01 -0300

    navbar, cattegorias y buscador

--- src/component/Grid.js
+++ src/component/Grid.js
@@ -3,42 +3,32 @@ import { Link } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import ProductAddToCart from "../commons/ProductAddToCart";
-import {useNavigate} from "react-router-dom" 
+import { useNavigate } from "react-router-dom";
 
-function GridGeneral() {
-  const [products, setProducts] = useState([]);
+function GridGeneral({ products }) {
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleClick = (id) => {
     axios
-    .get(`/products/${id}`)
-    .then((result) => result.data)
-    .then(() => {
-      navigate(`products/${id}`)
-      
-    })
-    .catch((err) => console.log(err));
-  }
-
-
-  useEffect(() => {
-    axios
-      .get("/products")
+      .get(`/products/${id}`)
       .then((result) => result.data)
-      .then((products) => {
-        setProducts(products);
+      .then(() => {
+        navigate(`/products/${id}`);
       })
       .catch((err) => console.log(err));
-  }, []);
-
+  };
 
   return (
     <SimpleGrid spacing="30px" minChildWidth="300px">
       {products?.map((product, i) => (
-         <Link to={`products/${product.id}`}> 
-        <ProductAddToCart onClick={() => handleClick(product.id)} key={i} data={product} />
-         </Link> 
+        <Link to={`/products/${product.id}`}>
+          <ProductAddToCart
+            onClick={() => handleClick(product.id)}
+            key={i}
+            data={product}
+          />
+        </Link>
       ))}
     </SimpleGrid>
   );

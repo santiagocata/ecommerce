db7bb6264092c84688b2c776d9f8b777b2cfc9aa
santiagocata <santiagoezequielcata@gmail.com>
2022-03-15 20:53:39 -0300

    sedeo terminado

--- api/config/lists/usersList.js
+++ api/config/lists/usersList.js
@@ -5,24 +5,63 @@ const usersList = [
     name: "React",
     lastName: "Sport",
     address: "abc 120",
-    
   },
   {
-    email: "user1@gmail.com",
+    email: "user1@fake.com",
     password: "12345678",
     name: "Lionel",
     lastName: "Messi",
     address: "abc 123",
-    
   },
   {
-    email: "user2@gmail.com",
+    email: "user2@fake.com",
     password: "12345678",
     name: "Neymar",
-    lastName: "Jr",
+    lastName: "Junior",
     address: "abc 124",
-    
-  }
+  },
+  {
+    email: "user3@fake.com",
+    password: "12345678",
+    name: "Carlos",
+    lastName: "Tevez",
+    address: "abc 124",
+  },
+  {
+    email: "user4@fake.com",
+    password: "12345678",
+    name: "Moria",
+    lastName: "Casan",
+    address: "abc 124",
+  },
+  {
+    email: "user5@fake.com",
+    password: "12345678",
+    name: "Maru",
+    lastName: "Botana",
+    address: "abc 124",
+  },
+  {
+    email: "user6@fake.com",
+    password: "12345678",
+    name: "Guido",
+    lastName: "Kazka",
+    address: "abc 124",
+  },
+  {
+    email: "user7@fake.com",
+    password: "12345678",
+    name: "Juan",
+    lastName: "Perez",
+    address: "abc 124",
+  },
+  {
+    email: "user8@fake.com",
+    password: "12345678",
+    name: "Pedro",
+    lastName: "Gonzales",
+    address: "abc 124",
+  },
 ];
 
 module.exports = usersList;
